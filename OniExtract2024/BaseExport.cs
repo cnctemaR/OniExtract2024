db868567364ef1868699bcb71770eac7117b012d
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-05-21 17:41:29 +0800

    add dlcs

--- OniExtract2024/BaseExport.cs
+++ OniExtract2024/BaseExport.cs
@@ -9,10 +9,32 @@ namespace OniExtract2024
         public virtual string DatabaseDirName { get; set; } = "database";
         public virtual string ExportFileName { get; set; } = "database";
         public string buildVersion = BuildWatermark.GetBuildText();
+        public string[] dlcs = GetDlcs();
 
         public string GetDatabaseLocation()
         {
             return Path.Combine(Util.RootFolder(), "export", DatabaseDirName);
+        }
+
+        public static string[] GetDlcs()
+        {
+            bool base_only = DlcManager.IsDlcListValidForCurrentContent(DlcManager.AVAILABLE_VANILLA_ONLY);
+            bool dlc1_only = DlcManager.IsDlcListValidForCurrentContent(DlcManager.AVAILABLE_EXPANSION1_ONLY);
+            bool all_available = DlcManager.IsDlcListValidForCurrentContent(DlcManager.AVAILABLE_ALL_VERSIONS);
+            if (all_available)
+            {
+                if (base_only) return DlcManager.AVAILABLE_VANILLA_ONLY;
+                else return DlcManager.AVAILABLE_ALL_VERSIONS;
+            }
+            else if (base_only)
+            {
+                return DlcManager.AVAILABLE_VANILLA_ONLY;
+            }
+            else if (dlc1_only)
+            {
+                return DlcManager.AVAILABLE_EXPANSION1_ONLY;
+            }
+            return null;
         }
 
         public void ExportJsonFile()
