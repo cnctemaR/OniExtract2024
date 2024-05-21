5b7329a49ea483dfe88db8e1f78b6bc6904eaca0
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-05-21 18:55:22 +0800

    add geyser export

--- OniExtract2024/Patches.cs
+++ OniExtract2024/Patches.cs
@@ -1,4 +1,6 @@
 using HarmonyLib;
+using static GeyserGenericConfig;
+using System.Collections.Generic;
 
 namespace OniExtract2024
 {
@@ -10,6 +12,18 @@ namespace OniExtract2024
             private static void Postfix()
             {
 
+            }
+        }
+
+        [HarmonyPatch(typeof(GeyserGenericConfig), "GenerateConfigs")]
+        internal class OniExtract_Game_Geysers
+        {
+            static void Postfix(ref List<GeyserPrefabParams> __result)
+            {
+                Debug.Log("OniExtract: " + "Export Geysers");
+                ExportGeyser exportGeyser = new ExportGeyser();
+                exportGeyser.AddGeyserPrefabParams(__result);
+                exportGeyser.ExportJsonFile();
             }
         }
     }
