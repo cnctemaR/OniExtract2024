40ce4817fc40ae647fb31956df2aae147d92afdd
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-05-21 19:05:28 +0800

    add recipe export

---
+++ OniExtract2024/ExportRecipe.cs
@@ -0,0 +1,20 @@
+﻿using OniExtract2024;
+using System.Collections.Generic;
+
+public class ExportRecipe : BaseExport
+{
+    public override string ExportFileName { get; set; } = "recipe";
+    public List<ComplexRecipe> recipes = new List<ComplexRecipe>();
+
+    public ExportRecipe()
+	{
+    }
+
+    public void ExportComplexRecipes()
+    {
+        foreach (var mRecipe in ComplexRecipeManager.Get().recipes)
+        {
+            this.recipes.Add(mRecipe);
+        }
+    }
+}
--- OniExtract2024/Patches.cs
+++ OniExtract2024/Patches.cs
@@ -10,6 +10,10 @@ namespace OniExtract2024
             private static void Postfix()
             {
 
+                Debug.Log("OniExtract: " + "Export recipes");
+                ExportRecipe exportRecipe = new ExportRecipe();
+                exportRecipe.ExportComplexRecipes();
+                exportRecipe.ExportJsonFile();
             }
         }
     }
