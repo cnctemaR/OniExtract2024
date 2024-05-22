70b0116970bca4bec1d345af570322542746d272
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-05-22 13:57:16 +0800

    ignore Debug

--- OniExtract2024/Patches.cs
+++ OniExtract2024/Patches.cs
@@ -10,7 +10,7 @@ namespace OniExtract2024
             private static void Postfix()
             {
 
-                Debug.Log("OniExtract: " + "Export Tags");
+                //Debug.Log("OniExtract: " + "Export Tags");
                 ExportTag exportTag = new ExportTag();
                 exportTag.AddAllGameTags();
                 exportTag.ExportJsonFile();
