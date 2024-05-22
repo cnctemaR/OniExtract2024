019c8e8e7de76c2a4d082a4835cc4bedb515b43d
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-05-22 13:57:31 +0800

    ignore Debug

--- OniExtract2024/Patches.cs
+++ OniExtract2024/Patches.cs
@@ -10,7 +10,7 @@ namespace OniExtract2024
             private static void Postfix()
             {
 
-                Debug.Log("OniExtract: " + "Export PO_string");
+                //Debug.Log("OniExtract: " + "Export PO_string");
                 ExportPOString exportPOString = new ExportPOString();
                 exportPOString.ExportAll();
                 exportPOString.ExportJsonFile();
