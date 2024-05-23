0b58f546196520474427696d46f34d01c20abd12
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-05-23 19:31:09 +0800

    update

---
+++ OniExtract2024/model/OutWiltConditions.cs
@@ -0,0 +1,20 @@
+﻿using Klei.AI;
+
+namespace OniExtract2024
+{
+    public class OutWiltConditions
+    {
+        public float WiltDelay = 1f;
+        public float RecoveryDelay = 1f;
+        public float minRad = 0f;
+        public float maxRad = float.NaN;
+
+        public OutWiltConditions(WiltCondition wiltable, Modifiers componentMods)
+        {
+            this.WiltDelay = wiltable.WiltDelay;
+            this.RecoveryDelay = wiltable.RecoveryDelay;
+            this.minRad = componentMods.GetPreModifiedAttributeValue(Db.Get().PlantAttributes.MinRadiationThreshold);
+            this.maxRad = componentMods.GetPreModifiedAttributeValue(Db.Get().PlantAttributes.MaxRadiationThreshold);
+        }
+    }
+}
