17a125ed88a2b7457be4cee67048e9343ebe4c35
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-05-23 17:18:17 +0800

    update

--- OniExtract2024/Patches.cs
+++ OniExtract2024/Patches.cs
@@ -1,10 +1,31 @@
-using HarmonyLib;
+﻿using HarmonyLib;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace OniExtract2024
 {
     public class Patches
     {
+        [HarmonyPatch(typeof(ComplexRecipeManager))]
+        [HarmonyPatch("Add")]
+        public class ComplexRecipeManager_Add_Patch
+        {
+            // 在调用 Add 方法之前进行前置检查
+            static bool Prefix(ComplexRecipe recipe, List<ComplexRecipe> ___recipes)
+            {
+                foreach (ComplexRecipe recipe2 in ___recipes)
+                {
+                    if (recipe2.id == recipe.id)
+                    {
+                        // 在到达 Debug.LogError 之前拦截并跳过该方法的执行
+                        return false;
+                    }
+                }
+                // 继续执行原始方法
+                return true;
+            }
+        }
+
         [HarmonyPatch(typeof(EntityConfigManager), "RegisterEntity")]
         internal class OniExtract_Game_EntityConfig
         {
