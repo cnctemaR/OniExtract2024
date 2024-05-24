8f5d0effeb3eaef890b311ecaea4be136ef651a7
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-05-24 21:44:43 +0800

    bugfix

--- OniExtract2024/ExportElement.cs
+++ OniExtract2024/ExportElement.cs
@@ -1,10 +1,11 @@
 ﻿using OniExtract2024;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class ExportElement : BaseExport
 {
     public override string ExportFileName { get; set; } = "elements";
-    public Dictionary<int, Element> elementTable;
+    public Dictionary<int, Element> elementTable = new Dictionary<int, Element>();
 
     public ExportElement()
     {
@@ -12,6 +13,23 @@ public class ExportElement : BaseExport
 
     public void AddAllElement()
     {
-        this.elementTable = ElementLoader.elementTable;
+        foreach (Element element in ElementLoader.elements)
+        {
+            Debug.Log(element.tag.Name);
+            this.elementTable[((int)element.id)] = element;
+
+            Substance substance = this.elementTable[((int)element.id)].substance;
+            if (substance != null)
+            {
+                if (substance.material != null)
+                {
+                    if (!substance.material.HasProperty("_Color"))
+                    {
+                        Debug.Log("No Property: " + element.tag.Name);
+                        this.elementTable[((int)element.id)].substance.material.color = Color.clear;
+                    }                    
+                }
+            }
+        }
     }
 }
