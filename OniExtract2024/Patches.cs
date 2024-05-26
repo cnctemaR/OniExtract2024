3f0ac19ad9f87e8de00bae5d5e49695cab466a35
cnctemaR <42052819+cnctemaR@users.noreply.github.com>
2024-05-26 19:57:11 +0800

    update export function

--- OniExtract2024/Patches.cs
+++ OniExtract2024/Patches.cs
@@ -9,32 +9,13 @@ namespace OniExtract2024
 {
     public class Patches
     {
-        [HarmonyPatch(typeof(ComplexRecipeManager))]
-        [HarmonyPatch("Add")]
-        public class ComplexRecipeManager_Add_Patch
-        {
-            // 在调用 Add 方法之前进行前置检查
-            static bool Prefix(ComplexRecipe recipe, List<ComplexRecipe> ___recipes)
-            {
-                foreach (ComplexRecipe recipe2 in ___recipes)
-                {
-                    if (recipe2.id == recipe.id)
-                    {
-                        // 在到达 Debug.LogError 之前拦截并跳过该方法的执行
-                        return false;
-                    }
-                }
-                // 继续执行原始方法
-                return true;
-            }
-        }
+        static ExportEntity exportEntity = new ExportEntity();
 
         [HarmonyPatch(typeof(EntityConfigManager), "RegisterEntity")]
         internal class OniExtract_Game_EntityConfig
         {
             private static readonly MethodInfo InjectBehind = AccessTools.Method(typeof(IEntityConfig), nameof(IEntityConfig.CreatePrefab));
             private static readonly MethodInfo RegisterExportEntityMethod = AccessTools.Method(typeof(OniExtract_Game_EntityConfig), nameof(OniExtract_Game_EntityConfig.RegisterPatch));
-            static ExportEntity exportEntity = new ExportEntity();
             static string[] tempDlcs = null;
 
             public static void Prefix(IEntityConfig config)
@@ -51,7 +32,6 @@ namespace OniExtract2024
                 };
                 ExportEntity.LoadEntityComponent(gameObject, bEntity);
                 exportEntity.entities.Add(bEntity);
-                exportEntity.ExportJsonFile();
                 return gameObject;
             }
 
@@ -73,7 +53,8 @@ namespace OniExtract2024
         {
             private static void Postfix()
             {
-
+                //Debug.Log("OniExtract: " + "Export Food");
+                exportEntity.ExportJsonFile();
             }
         }
     }
